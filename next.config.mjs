72002b7e746bf32dd931a8e39c5f94458a713a86
harsh kumar <digitallifeharsh@gmail.com>
2026-09-25 19:10:16 +0530

    Proxy backend course images through website

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/backend-api/uploads/:path*",
+        destination: "http://15.252.146.207:5000/uploads/:path*",
+      },
+      {
         source: "/backend-api/:path*",
         destination: "http://15.252.146.207:5000/api/v1/:path*",
       },
